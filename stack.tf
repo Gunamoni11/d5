ac92c13602fafa02965f53d10a5a5539581e77c3
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-02-21 15:47:52 +0530

    Update stack.tf

--- stack.tf
+++ stack.tf
@@ -2,6 +2,6 @@ provider "aws"{
   region = "us-east-2"
 }
 resource "aws_cloudformation_stack" "network" {
-  name = "networking-stack"
+  name = "networking-stack1"
   template_body = file("${path.module}/example.yml")
 }
